c36af94a102ba7bdcfa085e78be1100cab0425ca
Rico Angell <rangell@cs.umass.edu>
2023-02-19 11:27:38 -0500

    implemented slow version of SpecFW

--- main.py
+++ main.py
@@ -93,6 +93,71 @@ def cgal_primal_step(X, y, eps, t):
     X = (1-eta)*X + eta*X_update_dir
     return X
 
+def spec_fw(X, y, k, eps, max_steps):
+    for i in range(max_steps):
+        grad = C + A_adjoint(y) + A_adjoint(A_operator(X) - b)
+        _, V = scipy.sparse.linalg.eigsh(grad, k=k, which="SA")
+
+        # how should we initialize these?
+        step_size = 0.1
+        S = np.zeros((k, k))
+        eta = 1
+        for j in range(max_steps):
+            grad_S = (V.T @ C @ V
+                      + V.T @ A_adjoint(y) @ V
+                      + V.T @ A_adjoint(A_operator((eta*X) + (V @ S @ V.T)) - b) @ V)
+            grad_eta = (np.trace(C @ X)
+                        + np.dot(y, A_operator(X))
+                        + eta * np.linalg.norm(A_operator(X))**2
+                        + np.dot(A_operator(X), A_operator(V @ S @ V.T) - b))
+            S_update_dir = S - (step_size * grad_S)
+            eta_update_dir = eta - (step_size * grad_eta)
+
+            eigvals, eigvecs = np.linalg.eigh(S_update_dir)
+            eta_index = np.sum(eigvals < eta_update_dir)
+            eigvals = np.insert(eigvals, eta_index, eta_update_dir)
+
+            # project eigvals onto the n-dim simplex
+            descend_vals = np.flip(eigvals)
+            weighted_vals = (descend_vals
+                            + (1.0 / np.arange(1, len(descend_vals)+1))
+                                * (1 - np.cumsum(descend_vals)))
+            idx = np.sum(weighted_vals > 0) - 1
+            offset = weighted_vals[idx] - descend_vals[idx]
+            proj_descend_vals = descend_vals + offset
+            proj_descend_vals = proj_descend_vals * (proj_descend_vals > 0)
+            proj_eigvals = np.flip(proj_descend_vals)
+
+            eta_new = proj_eigvals[eta_index]
+            proj_eigvals = np.delete(proj_eigvals, eta_index)
+            S_new = (eigvecs * proj_eigvals[None, :]) @ eigvecs.T
+
+            #print('\t', np.max(np.abs(S_new - S)), np.abs(eta - eta_new))
+            if np.max(np.abs(S_new - S)) < eps and np.abs(eta - eta_new) < eps:
+                break
+
+            try:
+                assert np.isclose(np.trace(S_new) + eta_new, 1)
+            except:
+                print("ASSERTION THROWN!!!!")
+                embed()
+                exit()
+
+            S = S_new
+            eta = eta_new
+        
+        X_new = eta * X + V @ S @ V.T
+        if np.max(np.abs(X - X_new)) < eps:
+            break
+        #print(aug_Lagrangian(X_new, y), np.max(np.abs(X - X_new)))
+
+        X = X_new
+
+    return X
+
+
+
+
 eps = 1e-4
 
 # Define operator functions for AL methods.
@@ -112,13 +177,16 @@ for t in range(max_steps):
     # primal step
     #X = pgd(X, y, eps, max_steps)
     #X = fw(X, y, eps, max_steps)
-    X = cgal_primal_step(X, y, eps, t)
+    #X = cgal_primal_step(X, y, eps, t)
+
+    k = 2
+    X = spec_fw(X, y, k, eps, max_steps)
 
     # dual step
     y_prev = y
     y = y_prev + A_operator(X) - b
 
-    #print(np.max(np.abs(y - y_prev)))
+    print(np.max(np.abs(y - y_prev)))
 
     # check convergence
     if np.max(np.abs(y - y_prev)) < eps:
