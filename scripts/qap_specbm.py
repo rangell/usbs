bb320e74532f50db1a4c3f71f6de74f43d2584ad
Rico Angell <rangell@cs.umass.edu>
2023-06-30 11:01:15 -0400

    some tuning

--- scripts/qap_specbm.py
+++ scripts/qap_specbm.py
@@ -1,8 +1,10 @@
+import argparse
 import jax
 from jax import lax
 from jax._src.typing import Array
 from jax.experimental.sparse import BCOO
 import jax.numpy as jnp
+import json
 import numpy as np
 from scipy.spatial.distance import pdist, squareform  # type: ignore
 from typing import Any, Callable, Tuple
@@ -268,17 +270,40 @@ def create_qap_round(l: int, D: Array, W: BCOO) -> Callable[[BCOO, Array, Array]
     return qap_round
 
 
+def get_hparams():
+    parser = argparse.ArgumentParser() 
+    parser.add_argument("--data_path", type=str, required=True, help="path to mat file")
+    parser.add_argument("--max_iters", type=int, required=True,
+                        help="number of iterations to run solver")
+    parser.add_argument("--k_curr", type=int, default=1,
+                        help="number of new eigenvectors to compute")
+    parser.add_argument("--k_past", type=int, default=0,
+                        help="number of new eigenvectors to compute")
+    parser.add_argument("--trace_factor", type=float, default=1.0,
+                        help="how much space to give trace")
+    parser.add_argument("--rho", type=float, default=0.1,
+                        help="proximal parameter")
+    parser.add_argument("--beta", type=float, default=0.25,
+                        help="sufficient decrease parameter")
+    hparams = parser.parse_args()
+    return hparams
+
+
 if __name__ == "__main__":
     jax.config.update("jax_enable_x64", True)
 
-    DATAFILE = "data/qap/qapdata/chr12a.dat"
+    # get experiment hparams and print them out
+    hparams = get_hparams()
+    print(json.dumps(vars(hparams), indent=4))
+
+    #DATAFILE = "data/qap/qapdata/chr12a.dat"
     #DATAFILE = "data/qap/tspdata/ulysses16.tsp"
     #DATAFILE = "data/qap/tspdata/dantzig42.tsp"
     #DATAFILE = "data/qap/tspdata/bayg29.tsp"
     #DATAFILE = "data/qap/tspdata/bays29.tsp"
     #DATAFILE = "data/qap/tspdata/att48.tsp"
 
-    # TODO: write load TSP
+    DATAFILE = hparams.data_path
     if DATAFILE.split(".")[-1] == "dat":
         l, D, W, C = load_and_process_qap(DATAFILE)
     elif DATAFILE.split(".")[-1] == "tsp":
@@ -314,10 +339,10 @@ if __name__ == "__main__":
     tr_X = 0.0
     primal_obj = 0.0
 
-    trace_ub = 1.0 * float(l + 1) * SCALE_X
+    trace_ub = hparams.trace_factor * float(l + 1) * SCALE_X
 
-    k_curr = 5
-    k_past = 1
+    k_curr = hparams.k_curr
+    k_past = hparams.k_past
 
     qap_round = create_qap_round(l, D, W)
 
@@ -337,15 +362,15 @@ if __name__ == "__main__":
         b=scaled_b,
         b_ineq_mask=b_ineq_mask,
         Omega=Omega,
-        rho=0.1,
-        beta=0.25,
+        rho=hparams.rho,
+        beta=hparams.beta,
         k_curr=k_curr,
         k_past=k_past,
         SCALE_C=SCALE_C,
         SCALE_X=SCALE_X,
         SCALE_A=SCALE_A,
         eps=1e-5,  # hparams.eps,
-        max_iters=1000,  # hparams.max_iters,
+        max_iters=hparams.max_iters,  # hparams.max_iters,
         lanczos_inner_iterations=min(n, 32),
         lanczos_max_restarts=100,  # hparams.lanczos_max_restarts,
         subprob_eps=1e-7,
