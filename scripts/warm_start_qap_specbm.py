f5909554f95c76cb57458e09ca57eaef903b4548
Rico Angell <rangell@cs.umass.edu>
2023-07-22 08:04:50 -0400

    debugging nan issue

--- scripts/warm_start_qap_specbm.py
+++ scripts/warm_start_qap_specbm.py
@@ -58,6 +58,7 @@ def get_hparams():
 
 if __name__ == "__main__":
     jax.config.update("jax_enable_x64", True)
+    jax.config.update("jax_debug_nans", True)
 
     # get experiment hparams and print them out
     hparams = get_hparams()
