760f7df617fdc03d8cb218da79763ab790dda652
Rico Angell <rangell@cs.umass.edu>
2023-08-16 09:16:58 -0400

    .

--- scripts/warm_start_ecc_specbm.py
+++ scripts/warm_start_ecc_specbm.py
@@ -951,9 +951,10 @@ if __name__ == '__main__':
     pred_clusterings = {}
     num_blocks = len(blocks_preprocessed)
 
-    #sub_blocks_preprocessed = {}
+    sub_blocks_preprocessed = {}
     #sub_blocks_preprocessed['a moore'] = blocks_preprocessed['a moore']
-    sub_blocks_preprocessed = blocks_preprocessed
+    sub_blocks_preprocessed['j taylor'] = blocks_preprocessed['j taylor']
+    #sub_blocks_preprocessed = blocks_preprocessed
 
     for i, (block_name, block_data) in enumerate(sub_blocks_preprocessed.items()):
         edge_weights = block_data['edge_weights']
