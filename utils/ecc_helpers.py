4bcadaf64cdcdc009e95fed5f54e0b6d6a38775d
Rico Angell <rangell@cs.umass.edu>
2023-08-31 13:56:08 -0400

    .

--- utils/ecc_helpers.py
+++ utils/ecc_helpers.py
@@ -437,33 +437,35 @@ def embed_match_add_constraint(
     Omega = old_sdp_state.Omega
     P = old_sdp_state.P
     if old_sdp_state.X is not None:
-        eigvals, eigvecs = jnp.linalg.eigh(old_sdp_state.X)
-        column_embeds = eigvecs[:, -num_pred_clusters:] * np.sqrt(eigvals[None, -num_pred_clusters:])
+        #eigvals, eigvecs = jnp.linalg.eigh(old_sdp_state.X)
+        #column_embeds = eigvecs[:, -num_pred_clusters:] * np.sqrt(eigvals[None, -num_pred_clusters:])
 
-        avg_pos_embed = jnp.mean(column_embeds[pos_columns], axis=0)
-        avg_pos_embed = avg_pos_embed / np.linalg.norm(avg_pos_embed)
-        column_embeds = column_embeds.at[pos_columns, :].set(jnp.zeros_like(avg_pos_embed))
+        #avg_pos_embed = jnp.mean(column_embeds[pos_columns], axis=0)
+        #avg_pos_embed = avg_pos_embed / np.linalg.norm(avg_pos_embed)
+        #column_embeds = column_embeds.at[pos_columns, :].set(jnp.zeros_like(avg_pos_embed))
 
-        if len(ortho_indices) > 0:
-            neg_col_embeds = column_embeds[neg_columns]
-            neg_col_embeds = neg_col_embeds / np.linalg.norm(neg_col_embeds, axis=1)[:, None]
-            neg_col_projs = np.dot(neg_col_embeds, avg_pos_embed)
-            neg_col_projs = neg_col_projs / (np.linalg.norm(neg_col_embeds, axis=1) ** 2)
-            neg_col_projs = neg_col_projs[:, None] * avg_pos_embed
-            neg_col_embeds = neg_col_embeds - neg_col_projs
-            neg_col_embeds = neg_col_embeds / np.linalg.norm(neg_col_embeds, axis=1)[:, None]
+        #if len(ortho_indices) > 0:
+        #    neg_col_embeds = column_embeds[neg_columns]
+        #    neg_col_embeds = neg_col_embeds / np.linalg.norm(neg_col_embeds, axis=1)[:, None]
+        #    neg_col_projs = np.dot(neg_col_embeds, avg_pos_embed)
+        #    neg_col_projs = neg_col_projs / (np.linalg.norm(neg_col_embeds, axis=1) ** 2)
+        #    neg_col_projs = neg_col_projs[:, None] * avg_pos_embed
+        #    neg_col_embeds = neg_col_embeds - neg_col_projs
+        #    neg_col_embeds = neg_col_embeds / np.linalg.norm(neg_col_embeds, axis=1)[:, None]
 
-        column_embeds = column_embeds.at[pos_cluster_points, :].set(
-            column_embeds[pos_cluster_points] + avg_pos_embed[None, :])
-        column_embeds = column_embeds / np.linalg.norm(column_embeds, axis=1)[:, None]
+        #column_embeds = column_embeds.at[pos_cluster_points, :].set(
+        #    column_embeds[pos_cluster_points] + avg_pos_embed[None, :])
+        #column_embeds = column_embeds / np.linalg.norm(column_embeds, axis=1)[:, None]
 
-        if len(ortho_indices) > 0:
-            column_embeds = column_embeds.at[neg_columns, :].set(neg_col_embeds)
+        #if len(ortho_indices) > 0:
+        #    column_embeds = column_embeds.at[neg_columns, :].set(neg_col_embeds)
 
-        # add the embed for the new ecc constraint
-        column_embeds = jnp.concatenate([column_embeds, avg_pos_embed[None, :]], axis=0)
+        ## add the embed for the new ecc constraint
+        #column_embeds = jnp.concatenate([column_embeds, avg_pos_embed[None, :]], axis=0)
 
-        X = column_embeds @ column_embeds.T
+        #X = column_embeds @ column_embeds.T
+        X = BCOO.fromdense(X)
+        X = BCOO((X.data, X.indices), shape=(n, n)).todense()
         z = apply_A_operator_mx(n, m, A_data, A_indices, X) 
     if old_sdp_state.P is not None:
         assert False
