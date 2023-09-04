4fc9d43c819179aa3a82121f6a6969351b0a3403
Rico Angell <rangell@cs.umass.edu>
2023-09-04 12:40:38 -0400

    .

--- utils/ecc_helpers.py
+++ utils/ecc_helpers.py
@@ -499,8 +499,8 @@ def column_drop_add_constraint(
         avg_embed = avg_embed / jnp.linalg.norm(avg_embed)
 
         #point_embeds = point_embeds.at[nbr_ecc_points].set(point_embeds[nbr_ecc_points] + avg_embed[None, :])
-        point_embeds = point_embeds.at[ecc_points].set(avg_embed[None, :])
-        #point_embeds = point_embeds.at[nbr_ecc_points].set(avg_embed[None, :])
+        #point_embeds = point_embeds.at[ecc_points].set(avg_embed[None, :])
+        point_embeds = point_embeds.at[nbr_ecc_points].set(avg_embed[None, :])
         point_embeds = jnp.concatenate([point_embeds, avg_embed[None, :]], axis=0)
 
         point_embeds = point_embeds / jnp.linalg.norm(point_embeds, axis=1)[:, None]
