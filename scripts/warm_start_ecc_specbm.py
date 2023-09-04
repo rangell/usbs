102f98bf7fb28c5ffceba2129f4017653d3b23f4
Rico Angell <rangell@cs.umass.edu>
2023-09-04 16:14:08 -0400

    .

--- scripts/warm_start_ecc_specbm.py
+++ scripts/warm_start_ecc_specbm.py
@@ -1046,9 +1046,9 @@ if __name__ == '__main__':
     num_blocks = len(blocks_preprocessed)
 
     sub_blocks_preprocessed = {}
-    sub_blocks_preprocessed['a moore'] = blocks_preprocessed['a moore']
+    #sub_blocks_preprocessed['a moore'] = blocks_preprocessed['a moore']
     #sub_blocks_preprocessed['j taylor'] = blocks_preprocessed['j taylor']
-    #sub_blocks_preprocessed['s patel'] = blocks_preprocessed['s patel']
+    sub_blocks_preprocessed['s patel'] = blocks_preprocessed['s patel']
     #sub_blocks_preprocessed['h evans'] = blocks_preprocessed['h evans']
     #sub_blocks_preprocessed['j kaiser'] = blocks_preprocessed['j kaiser']
     #sub_blocks_preprocessed = blocks_preprocessed
