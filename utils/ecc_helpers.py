cd0fc199d64226cd19b692c9bf31bb57be0c0620
Rico Angell <rangell@cs.umass.edu>
2023-09-04 16:33:56 -0400

    .

--- utils/ecc_helpers.py
+++ utils/ecc_helpers.py
@@ -537,7 +537,9 @@ def column_drop_add_constraint(
 
     #SCALE_A = jnp.ones_like(b)
 
-    y = jnp.zeros((m,)).at[jnp.arange(old_sdp_state.b.shape[0])].set(
+    #y = jnp.zeros((m,)).at[jnp.arange(old_sdp_state.b.shape[0])].set(
+    #    old_sdp_state.y / old_sdp_state.SCALE_A)
+    y = jnp.full((m,), SCALE_X).at[jnp.arange(old_sdp_state.b.shape[0])].set(
         old_sdp_state.y / old_sdp_state.SCALE_A)
     y = y * (SCALE_X / old_sdp_state.SCALE_X) * SCALE_A
 
