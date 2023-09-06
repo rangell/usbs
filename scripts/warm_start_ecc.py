75649f017478622578141f595a8c4856a7233d92
Rico Angell <rangell@cs.umass.edu>
2023-09-06 09:51:37 -0400

    warm-starting works in direct comparison on pubmed

--- scripts/warm_start_ecc.py
+++ scripts/warm_start_ecc.py
@@ -1036,9 +1036,9 @@ if __name__ == '__main__':
     #sub_blocks_preprocessed['a moore'] = blocks_preprocessed['a moore']
     #sub_blocks_preprocessed['j taylor'] = blocks_preprocessed['j taylor']
     #sub_blocks_preprocessed['s patel'] = blocks_preprocessed['s patel']
-    sub_blocks_preprocessed['h evans'] = blocks_preprocessed['h evans']
+    #sub_blocks_preprocessed['h evans'] = blocks_preprocessed['h evans']
     #sub_blocks_preprocessed['j kaiser'] = blocks_preprocessed['j kaiser']
-    #sub_blocks_preprocessed = blocks_preprocessed
+    sub_blocks_preprocessed = blocks_preprocessed
 
     for i, (block_name, block_data) in enumerate(sub_blocks_preprocessed.items()):
         edge_weights = block_data['edge_weights']
