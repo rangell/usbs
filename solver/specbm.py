5e7daa8b1521154e6bcc422b78b55490224329f8
Rico Angell <rangell@cs.umass.edu>
2023-09-15 12:40:37 -0400

    fixing constants on obj_gap and infeas_gap

--- solver/specbm.py
+++ solver/specbm.py
@@ -587,9 +587,10 @@ def specbm(
         bar_primal_obj_next = eta * state.bar_primal_obj
         bar_primal_obj_next += jnp.trace(curr_VSV_T_factor.T @ (state.C @ curr_VSV_T_factor))
         
-        obj_gap = jnp.abs(primal_obj_next + neg_obj_lb)
+        obj_gap = jnp.abs(primal_obj_next + neg_obj_lb) / (SCALE_C * SCALE_X)
+        obj_gap /= 0.001 + (jnp.abs(primal_obj_next) / (SCALE_C * SCALE_X))
         infeas_gap = jnp.linalg.norm((z_next - state.b + upsilon_next) / SCALE_A) / SCALE_X
-        infeas_gap /= 1.0 + jnp.linalg.norm((state.b / SCALE_A) / SCALE_X)
+        infeas_gap /= 0.001 + jnp.linalg.norm((state.b / SCALE_A) / SCALE_X)
         max_infeas = jnp.max(jnp.abs(z_next - state.b + upsilon_next) / SCALE_A) / SCALE_X
 
         if state.Omega is not None and callback_fn is not None:
