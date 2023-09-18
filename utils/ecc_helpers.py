3d7a0fcadfb9031519a3ad507dcf9550f497cf15
Rico Angell <rangell@cs.umass.edu>
2023-09-18 16:03:32 -0400

    .

--- utils/ecc_helpers.py
+++ utils/ecc_helpers.py
@@ -261,7 +261,7 @@ def warm_start_add_constraint(
         avg_embed = jnp.sum(point_embeds[ecc_points] / ecc_counts[:, None], axis=0)
         avg_embed = avg_embed / jnp.linalg.norm(avg_embed)
         point_embeds = point_embeds.at[ecc_points].set(avg_embed[None, :])
-        point_embeds = point_embeds.at[nbr_ecc_points].set(point_embeds[nbr_ecc_points] + avg_embed[None, :])
+        #point_embeds = point_embeds.at[nbr_ecc_points].set(point_embeds[nbr_ecc_points] + avg_embed[None, :])
         point_embeds = jnp.concatenate([point_embeds, avg_embed[None, :]], axis=0)
         point_embeds = point_embeds / jnp.linalg.norm(point_embeds, axis=1)[:, None]
         X = point_embeds @ point_embeds.T
@@ -280,8 +280,8 @@ def warm_start_add_constraint(
         old_sdp_state.y / old_sdp_state.SCALE_A)
     y = y * (SCALE_X / old_sdp_state.SCALE_X) * SCALE_A
 
-    # NOTE: this is proximal step: (1 / rho)*(AX - b)
-    y = y + ((1 / rho) * SCALE_X * jnp.clip(b - z, a_max=0.0))
+    ## NOTE: this is proximal step: (1 / rho)*(AX - b)
+    #y = y + ((1 / rho) * SCALE_X * jnp.clip(b - z, a_max=0.0))
 
     sdp_state = SDPState(
         C=C,
