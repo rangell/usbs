ac3b29d3e8b00148f9b65f3278a5d7bffa078d2d
Rico Angell <rangell@cs.umass.edu>
2023-09-18 12:25:52 -0400

    .

--- scripts/warm_start_ecc.py
+++ scripts/warm_start_ecc.py
@@ -966,7 +966,7 @@ def get_hparams() -> argparse.Namespace:
     # for constraint generation
     parser.add_argument('--max_rounds', type=int, default=100,
                         help="number of rounds to generate feedback for")
-    parser.add_argument('--max_overlap_feats', type=int, default=1,
+    parser.add_argument('--max_overlap_feats', type=int, default=2,
                         help="max num overlap features to sample.")
     hparams = parser.parse_args()
     return hparams
