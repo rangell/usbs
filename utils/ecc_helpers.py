a2c6a0e935ed17541c33f50dbf5a50f84852b097
Rico Angell <rangell@cs.umass.edu>
2023-09-20 11:48:19 -0400

    .

--- utils/ecc_helpers.py
+++ utils/ecc_helpers.py
@@ -243,7 +243,7 @@ def warm_start_add_constraint(
 
     neg_points = jnp.array([v for v, _ in ortho_indices])
 
-    #num_pred_clusters = max(jnp.unique(prev_pred_clusters).shape[0], 2)
+    num_pred_clusters = max(jnp.unique(prev_pred_clusters).shape[0], 2)
     #num_pred_clusters = old_n
 
     nbr_ecc_points = np.where(np.isin(prev_pred_clusters, prev_pred_clusters[ecc_points]))[0]
@@ -257,7 +257,7 @@ def warm_start_add_constraint(
     if old_sdp_state.X is not None:
         # compute rank-`num_pred_clusters` approximation of X
         eigvals, eigvecs = jnp.linalg.eigh(old_sdp_state.X)
-        num_pred_clusters = old_n
+        num_pred_clusters = 72
         print("embed dim: ", num_pred_clusters)
         point_embeds = (eigvecs[:,-num_pred_clusters:] * jnp.sqrt(eigvals[None, -num_pred_clusters:]))
         point_embeds = point_embeds / jnp.linalg.norm(point_embeds, axis=1)[:, None]
