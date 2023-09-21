131093adeb9d3c44367477d5ce1e23e52b0e915e
Rico Angell <rangell@cs.umass.edu>
2023-09-21 15:29:32 -0400

    .

--- utils/ecc_helpers.py
+++ utils/ecc_helpers.py
@@ -262,7 +262,7 @@ def warm_start_add_constraint(
         point_embeds = point_embeds / jnp.linalg.norm(point_embeds, axis=1)[:, None]
         avg_embed = jnp.sum(point_embeds[ecc_points] / ecc_counts[:, None], axis=0)
         avg_embed = avg_embed / jnp.linalg.norm(avg_embed)
-        point_embeds = point_embeds.at[nbr_ecc_points].set(point_embeds[nbr_ecc_points] + 0.5 * avg_embed[None, :])
+        #point_embeds = point_embeds.at[nbr_ecc_points].set(point_embeds[nbr_ecc_points] + 0.5 * avg_embed[None, :])
         #point_embeds = point_embeds.at[ecc_points].set(point_embeds[ecc_points] + avg_embed[None, :])
         #point_embeds = point_embeds.at[ecc_points].set(avg_embed[None, :])
         point_embeds = jnp.concatenate([point_embeds, avg_embed[None, :]], axis=0)
@@ -270,7 +270,9 @@ def warm_start_add_constraint(
         if neg_points.size > 0:
             point_embeds = point_embeds.at[neg_points].set(jnp.zeros_like(point_embeds[0]))
         X = point_embeds @ point_embeds.T
-        #X = jnp.zeros_like(X)
+
+        X = jnp.zeros_like(X)
+
         z = apply_A_operator_mx(n, m, A_data, A_indices, X) 
     if old_sdp_state.P is not None:
         assert False
