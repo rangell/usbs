5ab4124abd5055fa05596732f9e1265b705ba0a5
Rico Angell <rangell@cs.umass.edu>
2023-09-23 08:59:48 -0400

    .

--- scripts/warm_start_ecc.py
+++ scripts/warm_start_ecc.py
@@ -43,8 +43,8 @@ class EccClusterer(object):
 
         self.hparams = hparams
         self.edge_weights = edge_weights
-        self.edge_weights.data += 0.6
-        self.sparse_laplacian = create_sparse_laplacian(edge_weights=edge_weights, eps=0.75)
+        self.edge_weights.data += 0.7
+        self.sparse_laplacian = create_sparse_laplacian(edge_weights=edge_weights, eps=0.5)
 
         self.features = features
         self.n = self.features.shape[0]
