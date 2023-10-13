b3cce854cecb5e38fd0de2d70a19268845e137af
Rico Angell <rangell@cs.umass.edu>
2023-10-13 11:25:22 -0400

    .

--- utils/ecc_helpers.py
+++ utils/ecc_helpers.py
@@ -281,16 +281,21 @@ def warm_start_add_constraint(
     old_diag_indices = jnp.unique(old_sdp_state.A_indices[old_diag_mask][:, 0])
     avg_old_diag_val = jnp.mean(old_sdp_state.y[old_diag_indices])
 
-    #diag_mask = ((A_indices[:, 1] == A_indices[:, 2]) & (A_data == 1.0))
-    #diag_indices = jnp.unique(A_indices[diag_mask][:, 0])
-    #y = jnp.zeros((m,)).at[diag_indices].set(avg_old_diag_val)
+    diag_mask = ((A_indices[:, 1] == A_indices[:, 2]) & (A_data == 1.0))
+    diag_indices = jnp.unique(A_indices[diag_mask][:, 0])
+    y = jnp.zeros((m,)).at[diag_indices].set(avg_old_diag_val)
     #y = jnp.zeros((m,))
-    y = ((1 / (1.0 * rho)) * SCALE_X * jnp.clip(b - old_z, a_max=0.0))
+
+    # NOTE: this is proximal step: (1 / rho)*(AX - b)
+    y = y + ((1 / (1.0 * rho)) * SCALE_X * jnp.clip(b - old_z, a_max=0.0))
     y = y.at[jnp.arange(old_sdp_state.b.shape[0])].set(
         old_sdp_state.y / old_sdp_state.SCALE_A)
     y = y * (SCALE_X / old_sdp_state.SCALE_X) * SCALE_A
 
-    # NOTE: this is proximal step: (1 / rho)*(AX - b)
+    if n == 323:
+        embed()
+        exit()
+
 
     sdp_state = SDPState(
         C=C,
