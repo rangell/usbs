8050dd2f5459b0f7332cd8f40d2f21886a2ec654
Rico Angell <rangell@cs.umass.edu>
2023-10-15 09:22:10 -0400

    .

--- utils/ecc_helpers.py
+++ utils/ecc_helpers.py
@@ -273,8 +273,13 @@ def warm_start_add_constraint(
     old_diag_indices = jnp.unique(old_sdp_state.A_indices[old_diag_mask][:, 0])
     avg_old_diag_val = jnp.mean(old_sdp_state.y[old_diag_indices])
 
-    #diag_mask = ((A_indices[:, 1] == A_indices[:, 2]) & (A_data == 1.0))
-    #diag_indices = jnp.unique(A_indices[diag_mask][:, 0])
+    diag_mask = ((A_indices[:, 1] == A_indices[:, 2]) & (A_data == 1.0))
+    diag_indices = jnp.unique(A_indices[diag_mask][:, 0])
+
+    if 6 in ecc_points:
+        embed()
+        exit()
+
     #y = jnp.zeros((m,)).at[diag_indices].set(avg_old_diag_val)
     y = jnp.zeros((m,))
     y = y.at[jnp.arange(old_sdp_state.b.shape[0])].set(
