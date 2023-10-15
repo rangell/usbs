433835db4cc35e583615538317c90b8e8463b75d
Rico Angell <rangell@cs.umass.edu>
2023-10-15 12:46:51 -0400

    .

--- scripts/warm_start_ecc.py
+++ scripts/warm_start_ecc.py
@@ -568,7 +568,8 @@ def gen_ecc_constraint(point_feats: csr_matrix,
         )
         _sampled_pos_feats = np.where(gold_not_pred_sfc == np.max(gold_not_pred_sfc))[0]
         np.random.shuffle(_sampled_pos_feats)
-        sampled_pos_feats.append(_sampled_pos_feats[:2])
+        sampled_pos_feats.append(_sampled_pos_feats[0])
+        sampled_pos_feats.append(_sampled_pos_feats[1])
     sampled_pos_feats = np.asarray(sampled_pos_feats)
 
     # lastly, negative feats
@@ -586,7 +587,8 @@ def gen_ecc_constraint(point_feats: csr_matrix,
         )
         _sampled_neg_feats = np.where(pred_not_gold_sfc == np.max(pred_not_gold_sfc))[0]
         np.random.shuffle(_sampled_neg_feats)
-        sampled_neg_feats.append(_sampled_neg_feats[:2])
+        sampled_neg_feats.append(_sampled_neg_feats[0])
+        sampled_neg_feats.append(_sampled_neg_feats[1])
     sampled_neg_feats = np.asarray(sampled_neg_feats)
 
     # create the ecc constraint
