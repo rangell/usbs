e57357cddcd9b49cdac787ffb3fcda7590f23479
Rico Angell <rangell@cs.umass.edu>
2023-10-16 13:34:33 -0400

    .

--- utils/ecc_helpers.py
+++ utils/ecc_helpers.py
@@ -253,7 +253,7 @@ def warm_start_add_constraint(
         point_embeds = point_embeds / jnp.linalg.norm(point_embeds, axis=1)[:, None]
         avg_embed = jnp.sum(point_embeds[ecc_points] / ecc_counts[:, None], axis=0)
         avg_embed = avg_embed / jnp.linalg.norm(avg_embed)
-        point_embeds = point_embeds.at[ecc_points].set(avg_embed[None, :])
+        #point_embeds = point_embeds.at[ecc_points].set(avg_embed[None, :])
         point_embeds = jnp.concatenate([point_embeds, avg_embed[None, :]], axis=0)
         point_embeds = point_embeds / jnp.linalg.norm(point_embeds, axis=1)[:, None]
         if neg_points.size > 0:
@@ -277,10 +277,10 @@ def warm_start_add_constraint(
     old_diag_indices = jnp.unique(old_sdp_state.A_indices[old_diag_mask][:, 0])
     avg_old_diag_val = jnp.mean(old_sdp_state.y[old_diag_indices])
 
-    diag_mask = ((A_indices[:, 1] == A_indices[:, 2]) & (A_data == 1.0))
-    diag_indices = jnp.unique(A_indices[diag_mask][:, 0])
-    y = jnp.zeros((m,)).at[diag_indices].set(avg_old_diag_val)
-    #y = jnp.zeros((m,))
+    #diag_mask = ((A_indices[:, 1] == A_indices[:, 2]) & (A_data == 1.0))
+    #diag_indices = jnp.unique(A_indices[diag_mask][:, 0])
+    #y = jnp.zeros((m,)).at[diag_indices].set(avg_old_diag_val)
+    y = jnp.zeros((m,))
     y = y.at[jnp.arange(old_sdp_state.b.shape[0])].set(
         old_sdp_state.y / old_sdp_state.SCALE_A)
     y = y * (SCALE_X / old_sdp_state.SCALE_X) * SCALE_A
