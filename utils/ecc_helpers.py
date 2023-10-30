01071cc7b8ee58b6df7dac1d388f6c24c2330515
Rico Angell <rangell@cs.umass.edu>
2023-10-30 16:06:25 -0400

    .

--- utils/ecc_helpers.py
+++ utils/ecc_helpers.py
@@ -267,7 +267,7 @@ def warm_start_add_constraint(
     tr_X = jnp.trace(X)
     primal_obj = jnp.trace(C @ X)
 
-    constraint_scale_factor = 2.0
+    constraint_scale_factor = 10.0
 
     SCALE_X = 1.0 / float(n)
     SCALE_C = 1.0 / jnp.linalg.norm(C.data)  # equivalent to frobenius norm
@@ -291,6 +291,10 @@ def warm_start_add_constraint(
 
     # NOTE: this is proximal step: (1 / rho)*(AX - b)
     y = y + (SCALE_A / (1.0 * rho)) * SCALE_X * jnp.clip(b - z, a_max=0.0)
+
+    if np.isin(ecc_points, np.array([265, 268])):
+        embed()
+        exit()
 
     sdp_state = SDPState(
         C=C,
