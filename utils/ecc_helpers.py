02c7360f9ed5464e22f00914aa8219592be4544d
Rico Angell <rangell@cs.umass.edu>
2023-10-31 20:24:17 -0400

    .

--- utils/ecc_helpers.py
+++ utils/ecc_helpers.py
@@ -257,8 +257,8 @@ def warm_start_add_constraint(
         #point_embeds = point_embeds.at[ecc_points].set(avg_embed[None, :])
         point_embeds = jnp.concatenate([point_embeds, avg_embed[None, :]], axis=0)
         point_embeds = point_embeds / jnp.linalg.norm(point_embeds, axis=1)[:, None]
-        #if neg_points.size > 0:
-        #    point_embeds = point_embeds.at[neg_points].set(jnp.zeros_like(point_embeds[0]))
+        if neg_points.size > 0:
+            point_embeds = point_embeds.at[neg_points].set(jnp.zeros_like(point_embeds[0]))
         X = point_embeds @ point_embeds.T
         z = apply_A_operator_mx(n, m, A_data, A_indices, X) 
     if old_sdp_state.P is not None:
