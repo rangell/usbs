8e0f2d7c8d41631b9e7c886b4ea78bfaa768885b
Rico Angell <rangell@cs.umass.edu>
2023-11-06 16:16:37 -0500

    modifying maxcut large moderate experiments

--- misc/sbatch_templates/unity_gpu_large.sh
+++ misc/sbatch_templates/unity_gpu_large.sh
@@ -3,12 +3,12 @@
 #SBATCH --job-name=__job_name__
 #SBATCH --output=__out_path__.out
 #SBATCH -e __out_path__.err
-#SBATCH --partition=gypsum-rtx8000
+#SBATCH --partition=gypsum-1080ti
 #
 #SBATCH -N 1
 #SBATCH -n 8
 #SBATCH -G 1
-#SBATCH --mem=64G
+#SBATCH --mem=32G
 #SBATCH --time=48:10:00         
 
 export PYTHONPATH=$(pwd):$PYTHONPATH
