fba4260580bf57d8550f65f14b43c57883a94173
Rico Angell <rangell@cs.umass.edu>
2023-11-06 16:12:14 -0500

    add large maxcut moderate config and template

---
+++ misc/sbatch_templates/unity_gpu_large.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#SBATCH --job-name=__job_name__
+#SBATCH --output=__out_path__.out
+#SBATCH -e __out_path__.err
+#SBATCH --partition=gypsum-rtx8000
+#
+#SBATCH -N 1
+#SBATCH -n 8
+#SBATCH -G 1
+#SBATCH --mem=64G
+#SBATCH --time=48:10:00         
+
+export PYTHONPATH=$(pwd):$PYTHONPATH
+source ~/.bashrc
+eval "$(conda shell.bash hook)"
+conda activate specbm-gpu
+__cmd_str__
